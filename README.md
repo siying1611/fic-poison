78b7124c9e4cd5e70917d93edd0a97d6f26a25a0
TricksOfLoki <153242836+TricksOfLoki@users.noreply.github.com>
2025-05-25 12:44:35 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,37 @@
+# Fic Poisoner
+
+A small script that injects random, garbage words into an Ao3 fic, or any piece of HTML-formatted text.  Junk text uses HTML classes that can be hidden with CSS.
+
+### Preparing your fic
+
+Always keep a backup of your unpoisoned fic, in case you need to revert or make changes later.
+
+Go to Ao3's skins page.  Either create a new work skin, or edit the skin currently being used for your fic.  Add this section of code to the skin:
+
+```
+#workskin .poison {
+  display: none;
+}
+```
+
+Go to your fic's edit page.  Make sure that the work skin you just edited is selected for your fic in the Associations section.  Scroll down to the Work Text section, and ensure that the HTML option is selected, *not* Rich Text.  Copy the entire work or chapter, and paste it into a text document on your computer.  Save the document somewhere you can easily find it again.
+
+If you're copy and pasting a new work from another source, you can paste into the Rich Text editor and then switch to HTML, to allow Ao3 to automatically format your text.
+
+### Poisoning
+
+Run `add_poison.py`.  Input the path to the text file with the fic.  A poisoned copy will be made in the same folder as the original.
+
+### Run options
+
+- `file_path`: Required, the path to the file with your HTML-formatted fic.
+- `aggressive`: Optional, whether or not to enable aggressive mode.  Normal mode adds one paragraph of junk text in between each normal paragraph.  Aggressive mode inserts single words randomly throughout the text.
+- `poison_class`: Optional, name of the class to be used.  Defaults to "poison".  Remember to update your Ao3 work skin if you change this.
+
+### Updating/publishing your fic
+
+Copy the text from the newly generated file back into the HTML editor.  Remember to preview your fic to make sure that everything is working correctly before updating or publishing.
+
+You should also add an author's note letting readers know that downloads won't work correctly.  AI scrapers aren't likely to check the author's notes for every fic they scrape, so it should be safe to explain.
+
+#### Thanks to dolph for the word list from [their GitHub](https://github.com/dolph/dictionary)
---
+++ add_poison.py
@@ -0,0 +1,99 @@
+# Adds junk text in an attempt to make fics unuseable to train AI
+# By TricksOfLoki
+# Updated 5/25/2025
+
+from io import open
+from os.path import exists, isfile
+from random import randint
+from sys import argv
+from argparse import ArgumentParser
+
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument("filename", type=str, help="Input file location")
+    parser.add_argument("-m", "--mode", default="normal", help="normal or aggressive (default: normal)")
+    parser.add_argument("-c", "--class", default="poison", type=str, help="poison class name (default: poison)")
+
+    args = parser.parse_args()
+    config = vars(args)
+
+    return config["filename"], config["mode"], config["class"]
+
+
+def add_poison(filename:str, mode:str, poison_class:str):
+    # Check that input file exists
+    if not exists(filename) or not isfile(filename):
+        print("Invalid file name. Make sure file path is correct")
+        quit()
+
+    # Get word list
+    file = open("popular.txt", mode="r")
+    words = file.read()
+    words = words.split("\n")
+    wordslen = len(words)
+    file.close()
+
+    if mode == "normal":
+        # Get fic stored in HTML format
+        file = open(filename, mode="r", encoding="utf-8")
+        content = file.read()
+        file.close()
+
+        # Add a paragraph of junk text between every regular paragraph
+        split = content.split("</p>")
+        split.pop(len(split)-1) # Remove final empty item
+        content = ""
+        for item in split:
+            content += f"{item}</p>\n"
+            junk = ""
+            for i in range(0, randint(10, 100)):
+                junk += words[randint(0, wordslen)] + " "
+            content += f"<p class=\"{poison_class}\">{junk}</p>"
+        return content
+
+    elif mode == "aggressive":
+        # Get fic stored in HTML format
+        file = open(filename, mode="r", encoding="utf-8")
+        content = ""
+        next_char = " "
+        counting = True
+        count = 1
+
+        # Add spans of junk text at random intervals
+        while True:
+            next_char = file.read(1)
+            if not next_char:
+                break
+            content += next_char
+
+            if next_char == "<":
+                counting = False
+            elif next_char == ">":
+                counting = True
+
+            if counting:
+                count -= 1
+            if count <= 0:
+                junk = words[randint(0, wordslen)]
+                content += f"<span class=\"{poison_class}\">{junk}</span>"
+                count = randint(3, 30)
+                
+        file.close()
+        return content
+    
+    else:
+        print("Invalid mode. Try normal or aggressive")
+        quit()
+
+
+if __name__ == "__main__":
+    filename, mode, poison_class = parse_args()
+
+    content = add_poison(filename, mode, poison_class)
+
+    # Save new file next to old one
+    new_filename = filename.replace(".txt", "_poisoned.txt")
+    with open(new_filename, mode="w", encoding="utf-8") as new_file:
+        new_file.write(content)
+    print(f"Saved to {new_filename}")
